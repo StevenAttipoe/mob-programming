1c21f8464b1584f58f785f65f7fd3922b21d05bf
Steven <steven.attipoe@turntabl.io>
2022-07-06 12:33:35 +0000

    code refactored

--- src/main/java/PanagramCheck.java
+++ src/main/java/PanagramCheck.java
@@ -3,42 +3,25 @@ import java.util.HashSet;
 
 public class PanagramCheck {
     public static boolean isPanagram(String sentence) {
-
-        HashSet<Character> letters = new HashSet<>();
-
-        for (char letter: sentence.toCharArray()) {
-            if (Character.isAlphabetic(letter)) {
-                letters.add(Character.toLowerCase(letter));
-            }
-
-        }
-
-        return letters.size() == 26;
-
+        return checkPanagram(sentence) > -1;
     }
 
     public static int checkPanagram(String sentence){
 
         HashSet<Character> letters = new HashSet<>();
-        boolean flag = false;
+        boolean isDuplicate = false;
 
         for (char letter: sentence.toCharArray()) {
             if (Character.isAlphabetic(letter)) {
-                if (letters.contains(Character.toLowerCase(letter))) {
-                    flag = true;
+                if (!letters.add(Character.toLowerCase(letter))) {
+                    isDuplicate = true;
                 }
-                letters.add(Character.toLowerCase(letter));
             }
         }
 
-        if (letters.size() == 26 && flag) {
-            return 1;
+        if (letters.size() == 26) {
+            return isDuplicate ? 1 : 0;
         }
-        else if (letters.size() == 26 ) {
-            return 0;
-        }
-        else{
-            return -1;
-        }
+        return -1;
     }
 }
