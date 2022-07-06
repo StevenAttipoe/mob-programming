d09b06b782dfbb2365b87501b773b912f3fcccd7
Steven <steven.attipoe@turntabl.io>
2022-07-06 11:35:28 +0000

    third working version

--- src/test/java/PanagramTest.java
+++ src/test/java/PanagramTest.java
@@ -1,4 +1,4 @@
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
@@ -6,78 +6,78 @@ public class PanagramTest {
 
     @Test
     public void emptyIsNotAPanagram(){
-        Assert.assertFalse(PanagramCheck.isPanagram(""));
+        Assertions.assertFalse(PanagramCheck.isPanagram(""));
     }
 
     @Test
     public void alphabetIsPanagram(){
-        Assert.assertTrue(PanagramCheck.isPanagram("abcdefghijklmnopqrstuvwxyz"));
+        Assertions.assertTrue(PanagramCheck.isPanagram("abcdefghijklmnopqrstuvwxyz"));
     }
 
     @Test
     public void alphabetIsNotPanagram(){
-        Assert.assertFalse(PanagramCheck.isPanagram("abcdefghijklmnopqrstuvwwwww"));
+        Assertions.assertFalse(PanagramCheck.isPanagram("abcdefghijklmnopqrstuvwwwww"));
     }
 
     @Test
     public void alphabetCaseIsPanagram(){
-        Assert.assertTrue(PanagramCheck.isPanagram("AbcDefghIjKLmnOPqRSTUvwxyz"));
+        Assertions.assertTrue(PanagramCheck.isPanagram("AbcDefghIjKLmnOPqRSTUvwxyz"));
     }
 
     @Test
     public void alphabetCaseIsNotPanagram(){
-        Assert.assertFalse(PanagramCheck.isPanagram("bcDefzghIjKALmRSTUvwxy"));
+        Assertions.assertFalse(PanagramCheck.isPanagram("bcDefzghIjKALmRSTUvwxy"));
     }
 
     @Test
     public void alphabetPunctuatedIsPanagram(){
-        Assert.assertTrue(PanagramCheck.isPanagram("abcd;efgh,ijkl.mnop]qrst?uvw[xyz"));
+        Assertions.assertTrue(PanagramCheck.isPanagram("abcd;efgh,ijkl.mnop]qrst?uvw[xyz"));
     }
 
     @Test
     public void alphabetPunctuatedIsNotPanagram(){
-        Assert.assertFalse(PanagramCheck.isPanagram("bd;efh,ijkl.mnp]qrst?uw[xyz"));
+        Assertions.assertFalse(PanagramCheck.isPanagram("bd;efh,ijkl.mnp]qrst?uw[xyz"));
     }
 
     @Test
     public void alphabetFrequencyIsPanagram(){
-        Assert.assertTrue(PanagramCheck.isPanagram("aabbccddeeffgghhiijjkkllmnopqqrrrsttuvwxxyz"));
+        Assertions.assertTrue(PanagramCheck.isPanagram("aabbccddeeffgghhiijjkkllmnopqqrrrsttuvwxxyz"));
     }
 
     @Test
     public void alphabetFrequencyIsNotPanagram(){
-        Assert.assertFalse(PanagramCheck.isPanagram("bbccddeeffgghhiijjkkllmnopqqrrrsttuvwxxyz"));
+        Assertions.assertFalse(PanagramCheck.isPanagram("bbccddeeffgghhiijjkkllmnopqqrrrsttuvwxxyz"));
     }
 
 
     @Test
     public void alphabetMixedIsPanagram(){
-        Assert.assertTrue(PanagramCheck.isPanagram("obzdefhivajklmnwpqrsctuxgy"));
+        Assertions.assertTrue(PanagramCheck.isPanagram("obzdefhivajklmnwpqrsctuxgy"));
     }
 
     @Test
     public void alphabetMixedIsNotPanagram(){
-        Assert.assertFalse(PanagramCheck.isPanagram("obzdefhivajkmnwpqrsctuxgy"));
+        Assertions.assertFalse(PanagramCheck.isPanagram("obzdefhivajkmnwpqrsctuxgy"));
     }
 
     @Test
     public void alphaNumericIsPanagram(){
-        Assert.assertTrue(PanagramCheck.isPanagram("32ab2c2defgh24i4jklmno44p5qrstu37vwxy23z"));
+        Assertions.assertTrue(PanagramCheck.isPanagram("32ab2c2defgh24i4jklmno44p5qrstu37vwxy23z"));
     }
 
     @Test
     public void alphaNumericIsNotPanagram(){
-        Assert.assertFalse(PanagramCheck.isPanagram("32ab2c2def24i4jklo44p5qrstu37vy23z"));
+        Assertions.assertFalse(PanagramCheck.isPanagram("32ab2c2def24i4jklo44p5qrstu37vy23z"));
     }
 
     @Test
     public void alphabetWithSpacesIsPanagram(){
-        Assert.assertTrue(PanagramCheck.isPanagram("a  b cd efg  hi jk l mno  pqr st u vw  x yz  "));
+        Assertions.assertTrue(PanagramCheck.isPanagram("a  b cd efg  hi jk l mno  pqr st u vw  x yz  "));
     }
 
     @Test
     public void alphabetWithSpacesIsNotPanagram(){
-        Assert.assertFalse(PanagramCheck.isPanagram("  b cd efg  hi jk l mno  pqr st u vw  x yz  "));
+        Assertions.assertFalse(PanagramCheck.isPanagram("  b cd efg  hi jk l mno  pqr st u vw  x yz  "));
     }
 
 
